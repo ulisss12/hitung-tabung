12eada70e4d77987f3f6db494bdd1882cafdc88f
ulisss12 <ssetiawti321@gmail.com>
2024-12-03 12:30:42 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,10 +8,10 @@ r = st.number_input("masukan jari - jari (cm):",0)
 t = st.number_input("masukan tinggi (cm):",0)
 
 if st.button("hitung volume", type="primary"):
-  loading = st.progres(0)
+  loading = st.progress(0)
   for i in range(100):
     time.sleep(0.01)
-    loading.prgres(i+1)
+    loading.progress(i+1)
     
   v = math.pi*(r**2)*t
   st.success(f"volume tabung adalah (v:.2f)")
